85f338f111f18e41c14ccb86046d9b1951861de2
alfarozy.an <mr.alfarozy.a.n@gmail.com>
2024-11-29 10:57:55 +0700

    fix[file-upload]: update include file for connection

--- file-upload/lab-1/edit.php
+++ file-upload/lab-1/edit.php
@@ -1,5 +1,8 @@
+<?php include 'getProfile.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,37 +21,37 @@
     </style>
 
 </head>
+
 <body>
 
-<div class="container">
-    <aside class="sidebar">
-        <p>Upload file dengan extension apapun</p>
+    <div class="container">
+        <aside class="sidebar">
+            <p>Upload file dengan extension apapun</p>
 
-    </aside>
+        </aside>
 
-    <div class="main-content">
-        <div class="filter">
-            <span>Profile</span>
+        <div class="main-content">
+            <div class="filter">
+                <span>Profile</span>
+            </div>
+
+            <div>
+                <?php include 'getProfile.php'; ?>
+                <form action="<?php echo $host; ?>/file-upload/lab-1/updateProfile.php" method="post" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Avatar</label>
+                        <input class="form-control" type="file" id="formFile" name="avatar">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button> | <a href="<?php echo $host; ?>/file-upload/"><button type="button" class="btn btn-outline-primary">Back</button></a>
+                </form>
+
+            </div>
+
+
         </div>
-
-        <div>
-            <?php
-            include 'getProfile.php';
-            ?>
-            <form action="<?php echo $host;?>/file-upload/lab-1/updateProfile.php" method="post" enctype="multipart/form-data">
-                <div class="mb-3">
-                    <label for="formFile" class="form-label">Avatar</label>
-                    <input class="form-control" type="file" id="formFile" name="avatar">
-                </div>
-
-                <button type="submit" class="btn btn-primary">Submit</button> | <a href="<?php echo $host;?>/file-upload/"><button type="button" class="btn btn-outline-primary">Back</button></a>
-            </form>
-
-        </div>
-
-
     </div>
-</div>
 
 </body>
+
 </html>
--- file-upload/lab-1/index.php
+++ file-upload/lab-1/index.php
@@ -1,5 +1,7 @@
+<?php include 'getProfile.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,39 +20,41 @@
     </style>
 
 </head>
+
 <body>
 
-<div class="container">
-    <aside class="sidebar">
-        <p>Pada studi kasus berikut peretas dapat mengupload file apapun ke server</p>
+    <div class="container">
+        <aside class="sidebar">
+            <p>Pada studi kasus berikut peretas dapat mengupload file apapun ke server</p>
 
 
-    </aside>
+        </aside>
 
-    <div class="main-content">
-        <?php echo $message; ?>
-        <div class="filter">
-            <span>Profile</span>
+        <div class="main-content">
+            <?php echo $message; ?>
+            <div class="filter">
+                <span>Profile</span>
+            </div>
+
+            <div>
+
+                <form>
+
+                    <div class="mb-3">
+                        <label for="formFile" class="form-label">Avatar:</label>
+                        <img src="<?php echo $host . "/assets/gallery/" . $avatar; ?>" alt="" width="30%">
+                    </div>
+                    <a href="<?php echo $host . '/file-upload/lab-1/edit.php'; ?>">
+                        <button type="button" class="btn btn-primary">Edit</button>
+                    </a> | <a href="<?php echo $host; ?>/file-upload/"><button type="button" class="btn btn-outline-primary">Back</button></a>
+                </form>
+
+            </div>
+
+
         </div>
-
-        <div>
-            <?php include 'getProfile.php';?>
-            <form>
-
-                <div class="mb-3">
-                    <label for="formFile" class="form-label">Avatar:</label>
-                    <img src="<?php echo $host."/assets/gallery/".$avatar;?>" alt="" width="30%">
-                </div>
-                <a href="<?php echo $host.'/file-upload/lab-1/edit.php';?>">
-                    <button type="button" class="btn btn-primary">Edit</button>
-                </a> | <a href="<?php echo $host;?>/file-upload/"><button type="button" class="btn btn-outline-primary">Back</button></a>
-            </form>
-
-        </div>
-
-
     </div>
-</div>
 
 </body>
+
 </html>
